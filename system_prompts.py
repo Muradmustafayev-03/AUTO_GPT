355fe4d1ab9fdacffe8c7b76bbd641168a8756ff
Murad <muradmustafayev03@gmail.com>
2023-08-21 14:43:46 +0400

    added system prompts

---
+++ system_prompts.py
@@ -0,0 +1,2 @@
+IS_TASK_COMPLICATED = 'Answer only "True" or "False"! The task given by the user is very complicated and requires multiple steps for completion?'
+DIVIDE_INTO_SUBTASKS = 'Give the steps for completing the task given by the user. Give response in the form: 1. Do this.\n2. Do that.\n3. Do something else.'
