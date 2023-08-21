d15cedca100feeca54ac0624e187f4fd1bac93f9
Murad <muradmustafayev03@gmail.com>
2023-08-21 14:48:58 +0400

    updated system prompts

--- system_prompts.py
+++ system_prompts.py
@@ -1,2 +1,5 @@
-IS_TASK_COMPLICATED = 'Answer only "True" or "False"! The task given by the user is very complicated and requires multiple steps for completion?'
-DIVIDE_INTO_SUBTASKS = 'Give the steps for completing the task given by the user. Give response in the form: 1. Do this.\n2. Do that.\n3. Do something else.'
+IS_TASK_COMPLICATED = 'Answer only "True" or "False"! ' \
+                      'The task given by the user is very complicated and requires multiple steps for completion?'
+DIVIDE_INTO_SUBTASKS = 'Give the steps for completing the task given by the user. ' \
+                       'Give response in the form: 1. Do this.\n2. Do that.\n3. Do something else. ' \
+                       'Note, that all the steps should be doable by a language model such as GPT-3.'
