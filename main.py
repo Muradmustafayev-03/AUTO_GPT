38725cb53dfbc46026f608fb0e4a0638c3228d72
Murad <muradmustafayev03@gmail.com>
2023-08-21 15:14:01 +0400

    solve complex tasks

--- main.py
+++ main.py
@@ -14,5 +14,15 @@ def divide_into_subtasks(user_prompt: str):
     return [match.group(2).strip() for match in matches]
 
 
-def solve_simple_task(user_prompt: str, background: str = None):
+def solve_simple_task(user_prompt: str, background: str = ''):
     return gpt_response(user_prompt, background)
+
+
+def solve_task(user_prompt: str, background: str = ''):
+    if not is_task_complicated(user_prompt):
+        return solve_simple_task(user_prompt, background)
+
+    subtasks = divide_into_subtasks(user_prompt)
+
+    for task in subtasks:
+        background += solve_task(task, background)
